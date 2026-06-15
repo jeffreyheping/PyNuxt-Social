6de74d2d6af6ad82dbdbafca0b2b19199e37079b
Jeffrey He <jeffreyhe@hotmail.com>
2026-06-15 13:45:45 +0800

    Update config.py

--- frontend/config.py
+++ frontend/config.py
@@ -5,7 +5,7 @@ import os
 DEBUG = True
 
 # 后端 API 地址
-API_BASE = os.getenv("API_BASE", "http://localhost:8000")
+API_BASE = os.getenv("API_BASE", "http://localhost:8012")
 
 # 站点名称
 SITE_NAME = "PyNuxt-Social"
